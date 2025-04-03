6a98409a70abcf8cec7c3da38fcf54f7d0938e8e
abishekchhetri <chhetriabishek143@gmail.com>
2025-04-03 14:42:06 +0545

    adding ../../ path though it is not detected for web to render svg

--- src/js/previewView.js
+++ src/js/previewView.js
@@ -1,5 +1,5 @@
 import { View } from './View';
-import icons from 'url:../img/icons.svg';
+import icons from 'url:../../img/icons.svg';
 
 class previewView extends View {
   _parentEl = '';
--- src/js/recipeView.js
+++ src/js/recipeView.js
@@ -1,4 +1,4 @@
-import icons from 'url:../img/icons.svg';
+import icons from 'url:../../img/icons.svg';
 import Fraction from 'fraction.js';
 import { View } from './View';
 
